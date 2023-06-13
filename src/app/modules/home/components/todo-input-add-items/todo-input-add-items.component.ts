ceb6c8991302883c88b0be82719e863360598e29
Marielle Mar <mariellemarsantos@gmail.com>
2023-06-13 12:42:49 -0400

    app to-do list

---
+++ src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input-add-items',
+  templateUrl: './todo-input-add-items.component.html',
+  styleUrls: ['./todo-input-add-items.component.scss']
+})
+export class TodoInputAddItemsComponent implements OnInit {
+  @Output() public emitItemTaskList = new EventEmitter();
+  
+  public addItemTaskList: string = "";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  public submitItemTaskList() {
+
+    this.addItemTaskList = this.addItemTaskList.trim();
+
+    if(this.addItemTaskList) {
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList="";
+    }
+    
+  }
+}
